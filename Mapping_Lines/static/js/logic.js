c2e6a810fe154149fdb7ef09a896c55a0f189095
babsonr1 <riley.babson@du.edu>
2021-11-30 21:59:41 -0700

    Removed comments.

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,8 +1,19 @@
 console.log("Working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // We create the tile layer that will be the background of our map.
+
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
 let cityData = cities;
 cityData.forEach(function(city) {
@@ -17,7 +28,7 @@ cityData.forEach(function(city) {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'streets-v11',
+    id: 'satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
